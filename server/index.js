3174223e9e00748943130ed32d673c62fd4dd822
Tufail Mehraj <shahtuffey@gmail.com>
2020-12-06 02:49:55 +0530

    mongoose srv ENOTFOUND error fixed

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://mern_project:mern_project123@cluster0.j2iaw.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://mern_project:mern_project123@cluster0.j2iaw.mongodb.net/dbname?retryWrites=true&w=majority'; // srv gives issues with moongoose, hence move mongoAtlas link for node < 2.2
+const CONNECTION_URL = 'mongodb://mern_project:mern_project123@cluster0-shard-00-00.j2iaw.mongodb.net:27017,cluster0-shard-00-01.j2iaw.mongodb.net:27017,cluster0-shard-00-02.j2iaw.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-y5krh0-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 // https://www.mongodb.com/cloud/atlas
